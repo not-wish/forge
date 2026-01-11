cce852c7d68050ab568785a442365921751b02bc
not-wish <visheshagarwaltsk@gmail.com>
2026-01-11 09:01:41 +0530

    Cleaned cli.py and added error handling in task.py for missing task.yaml file.

--- forge/cli.py
+++ forge/cli.py
@@ -2,26 +2,10 @@ import argparse
 from .hasher import hashContent
 from .task import build
 
-def add(args):
-    print(args.x + args.y)
-
-def subtract(args):
-    print(args.x - args.y)
-
-def multiply(args):
-    print(args.x * args.y)
-
-def divide(args):
-    if (args.y != 0):
-        print(args.x / args.y)
-    else:
-        print("Division by Zero: 0")
-
 def buildHelp(args):
     build(args.taskName)
 
-def hash(args):
-    
+def hash(args):   
     print(hashContent(args.fileName))
 
 def main():
@@ -29,30 +13,13 @@ def main():
     parser.add_argument('--version', action='version', version='forge v0.0.1')
     sub_parser = parser.add_subparsers(dest="OPTIONS", required=True)
 
-    add_p = sub_parser.add_parser("add")
-    add_p.add_argument("x", type=float)
-    add_p.add_argument("y", type=float)
-    add_p.set_defaults(func=add)
-    
-    sub_p = sub_parser.add_parser("subtract")
-    sub_p.add_argument("x", type=float)
-    sub_p.add_argument("y", type=float)
-    sub_p.set_defaults(func=subtract)
-    
-    mul_p = sub_parser.add_parser("multiply")
-    mul_p.add_argument("x", type=float)
-    mul_p.add_argument("y", type=float)
-    mul_p.set_defaults(func=multiply)
-    
-    div_p = sub_parser.add_parser("divide")
-    div_p.add_argument("x", type=float)
-    div_p.add_argument("y", type=float)
-    div_p.set_defaults(func=divide)
 
+    # Build Command
     com_p = sub_parser.add_parser("build")
     com_p.add_argument("taskName", type=str)
     com_p.set_defaults(func=buildHelp)
     
+    # Hash Command -> To check the hash of a file 
     hash_p = sub_parser.add_parser("hash")
     hash_p.add_argument("fileName", type=str)
     hash_p.set_defaults(func=hash)
--- forge/task.py
+++ forge/task.py
@@ -22,54 +22,58 @@ cache data model
 
 def build(taskName: str):
 
-    with open("task.yaml", "r") as f:
-        task = yaml.safe_load(f)["task"]
+    try:
+        with open("task.yaml", "r") as f:
+            task = yaml.safe_load(f)["task"]
 
-    if (task["name"] == taskName):
-        print(f"Task: {taskName} found!\n")
+        if (task["name"] == taskName):
+            print(f"Task: {taskName} found!\n")
 
-        curHash = list()
-        # calculate input hash
-        for i in task["inputs"]:
-            curHash.append(hashContent(i))
+            curHash = list()
+            # calculate input hash
+            for i in task["inputs"]:
+                curHash.append(hashContent(i))
 
-        flag = False
+            flag = False
 
-        if (os.path.exists("cache.json")):
-                # check with cache
-            with open("cache.json", "r") as c:
-                jsonData = c.read()
+            if (os.path.exists("cache.json")):
+                    # check with cache
+                with open("cache.json", "r") as c:
+                    jsonData = c.read()
 
-            cache = json.loads(jsonData)
-            # skip if matched
-            if (cache["input"] == len(curHash) and set(cache["inputHash"]) == set(curHash)):
-                print("\n-----------------\nSkipping command because there has been no changes in any input files\n-----------------\n")
-                
-                if (all(os.path.exists(o) for o in task["outputs"])):
-                    flag = True
-                else:
-                    print("\n-----------------\nRerunning command because some output files are missing in the directory\n-----------------\n")
+                cache = json.loads(jsonData)
+                # skip if matched
+                if (cache["input"] == len(curHash) and set(cache["inputHash"]) == set(curHash)):
+                    print("\n-----------------\nSkipping command because there has been no changes in any input files\n-----------------\n")
+                    
+                    if (all(os.path.exists(o) for o in task["outputs"])):
+                        flag = True
+                    else:
+                        print("\n-----------------\nRerunning command because some output files are missing in the directory\n-----------------\n")
 
-        if not flag:
-        # execute command if not matched
-            result = subprocess.run(
-                task["command"],
-                capture_output=True,
-                text=True
-            )
+            if not flag:
+            # execute command if not matched
+                result = subprocess.run(
+                    task["command"],
+                    capture_output=True,
+                    text=True
+                )
 
-            print(f"Output: {result.stdout}")
-            print("\n#---------------------------------#\n")
-            print(f"Errors: {result.stderr}")
+                print(f"Output: {result.stdout}")
+                print("\n#---------------------------------#\n")
+                print(f"Errors: {result.stderr}")
 
-            outputHash = list()
+                outputHash = list()
 
-            for i in task["outputs"]:
-                outputHash.append(hashContent(i))
+                for i in task["outputs"]:
+                    outputHash.append(hashContent(i))
 
-            catch(taskName, len(curHash), len(outputHash) , curHash, outputHash)
+                catch(taskName, len(curHash), len(outputHash) , curHash, outputHash)
 
 
-        # cache the new input   
-    else:
-        print("Task not found")
+            # cache the new input   
+        else:
+            print("Task not found")
+    
+    except FileNotFoundError:
+        print("------------------------------\ntask.yaml is not present in the current directory.\nPlease create a task.yaml file to define your tasks.\n------------------------------\n")
